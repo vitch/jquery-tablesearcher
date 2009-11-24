912257ac5a58da6384440fc85cf1239f135e6aeb
Kelvin Luck <git@kelvinluck.com>
2009-11-24 12:30:39 +0000

    Basic implementation of table search...

--- scripts/jquery.tablesearcher.js
+++ scripts/jquery.tablesearcher.js
@@ -13,16 +13,51 @@
 		{
 			s = $.extend({}, $.fn.tablesearcher.defaults, s);
 			return this.each(
-				function(i)
-				{
+				function(i){
+				
+					var searchTimeout;
+					var $table = $(this);
 					
+					if (s.input == null) {
+						throw new Error('Must provide an input element to bind to');
+					};
+					
+					s.input.bind('keyup', function(){
+						if (searchTimeout) {
+							clearTimeout(searchTimeout);
+						}
+						searchTimeout = setTimeout(initSearch, 300);
+					});
+					
+					var lastQ = '';
+					
+					function initSearch(){
+						var q = s.input.val().toLowerCase();
+						if (q == lastQ) 
+							return;
+						if (q == '') {
+							$table.find('tbody tr').css('display', '');
+						}
+						else {
+							//var tst = new RegExp(q, 'i');
+							var i = 0;
+							$table.find('tbody tr').each(function(){
+								var $tr = $(this);
+								var searchStr = $tr.text().toLowerCase();
+								//var isMatch = tst.test(searchStr);
+								var isMatch = searchStr.indexOf(q) > -1;
+								$tr.css('display', isMatch ? '' : 'none');
+							});
+						}
+						lastQ = q;
+						searchTimeout = undefined;
+					}
 				}
 			);
 		}
 	});
 	$.fn.tablesearcher.defaults = {
-		'blah' : 1,
-		'blaj' : 2
+		'input'	:	null
 	};
 	
 })(jQuery);
